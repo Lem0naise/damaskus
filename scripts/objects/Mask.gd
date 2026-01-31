ad6056f37c80db08280e764e0cc32c2e3a9587fa
Indigo Nolan <dev@indigonolan.com>
2026-01-31 13:32:40 +0000

    feat: Add crown and crownWin textures with corresponding import settings; update mask properties in Mask and Player scripts

--- scripts/objects/Mask.gd
+++ scripts/objects/Mask.gd
@@ -7,6 +7,14 @@ enum MaskType { NONE, DIMENSION, WATER, WINNER, BATTERING_RAM }
 # The Generator will set this variable before adding the child
 @export var mask_type: MaskType = MaskType.NONE
 
+
+@export var water_mask_still: Texture2D
+@export var water_mask_walking: Texture2D
+
+@export var win_mask_still: Texture2D
+@export var win_mask_walking: Texture2D
+
+
 func _ready():
 	# 1. Setup Visuals based on the type assigned by the LevelGenerator
 	setup_visuals()
@@ -19,7 +27,7 @@ func setup_visuals():
 	var label = Label.new()
 	label.position = Vector2(-28, -10)
 	label.size = Vector2(56, 20)
-	label.text = get_mask_name()
+	label.text =""
 	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	label.add_theme_font_size_override("font_size", 12)
 	add_child(label)
@@ -28,17 +36,24 @@ func setup_visuals():
 	# We use 'modulate' to tint the texture.
 	# Check if the node exists first to prevent crashes.
 	if has_node("TextureRect"):
-		$TextureRect.modulate = get_mask_color()
+		if mask_type == MaskType.WINNER:
+			$TextureRect.hide()
+			$CrownRect.show()
+			$CrownRect.texture = get_mask_texture()
+		else :
+			$TextureRect.show()
+			$CrownRect.hide()
+			$TextureRect.texture = get_mask_texture()
 	else:
 		print("Warning: No TextureRect found on Mask object")
 
-func get_mask_color() -> Color:
+func get_mask_texture() -> Texture2D:
 	match mask_type:
-		MaskType.DIMENSION: return Color(0.8, 0.2, 0.8, 1) # Purple
-		MaskType.WATER:     return Color(0.2, 0.6, 0.9, 1) # Blue
-		MaskType.WINNER:    return Color(0.827, 0.667, 0.326, 1.0)  # Blue for WATER
-		MaskType.BATTERING_RAM: return Color(0.8, 0.3, 0.2, 1) # Red/Orange
-		_:                  return Color(1, 1, 1, 0.5)
+		MaskType.DIMENSION: return null # Purple
+		MaskType.WATER:     return water_mask_still
+		MaskType.WINNER:    return win_mask_still
+		MaskType.BATTERING_RAM: return null
+		_:                  return null
 
 func get_mask_name() -> String:
 	match mask_type:
